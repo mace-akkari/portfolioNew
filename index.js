a46d2cddef5f46bfe917d6db038131d7e2dcab8e
Mace <maceplus@gmail.com>
2019-09-19 14:58:20 +0100

    made a function to allow all gallery pics to be cliked

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-const current = document.querySelector('#current');
-const imgs = document.querySelector('.imgs');
-const img = document.querySelectorAll('.imgs img');
-const opacity = 0.6;
+function gallary(el) {
+  const current = el.querySelector('.current');
+  const img = el.querySelectorAll('.imgs img');
+  const opacity = 0.6;
 
-// Set first img opacity
-img[0].style.opacity = opacity;
+  img[0].style.opacity = opacity;
+  img.forEach((el) => el.addEventListener('click', imgClick))
 
-imgs.addEventListener('click', imgClick);
+  function imgClick(e) {
+    // Reset the opacity
+    img.forEach(img => (img.style.opacity = 1));
 
-function imgClick(e) {
-  // Reset the opacity
-  img.forEach(img => (img.style.opacity = 1));
+    // Change current image to src of clicked image
+    current.src = e.target.src;
 
-  // Change current image to src of clicked image
-  current.src = e.target.src;
+    // Add fade in class
+    current.classList.add('fade-in');
 
-  // Add fade in class
-  current.classList.add('fade-in');
+    // Remove fade-in class after .5 seconds
+    setTimeout(() => current.classList.remove('fade-in'), 500);
 
-  // Remove fade-in class after .5 seconds
-  setTimeout(() => current.classList.remove('fade-in'), 500);
-
-  // Change the opacity to opacity var
-  e.target.style.opacity = opacity;
+    // Change the opacity to opacity var
+    e.target.style.opacity = opacity;
+  }
 }
+
+document.querySelectorAll('.img_container').forEach(el => gallary(el))
